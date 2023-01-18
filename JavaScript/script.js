abe59df53f9365cf42fcbafd41a77eb3e58d8dc0
EshanKhanna <khannaeshan@yahoo.in>
2023-01-19 00:22:53 +0530

    Commit done on 19-01-2023

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+function display(){
+    alert("hi");
+    display1();
+
+    var x = 10;
+    x = "Hello";
+    x = true;
+
+document.write(x);
+
+
+
+}
+
+
+function display(){
+    alert("called from display");
+}
